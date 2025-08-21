af65df35ce1987ebe7b081da7926920270901598
Arthur Duarte <arthureaux69@gmail.com>
2025-08-21 12:36:00 -0300

    Feat: Add database connection and data retrieval loop for Bitcoin and commodities

---
+++ getPrices_loop_db.py
@@ -0,0 +1,47 @@
+import time
+import pandas as pd
+from sqlalchemy import create_engine
+from getBitcoin import get_bitcoin_df
+from getComm import get_comm_df
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Configurações do banco de dados
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
+
+# criar conexão com o banco de dados
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+engine = create_engine(DATABASE_URL)
+
+# testar a conexão
+try:
+    with engine.connect() as connection:
+        print("Conexão bem sucedida!")
+except Exception as e:
+    print(f"Falha na conexão: {e}")
+
+SLEEP_SECONDS = 60
+
+if __name__ == "__main__":
+    while True:
+        # Coletar
+        df_btc = get_bitcoin_df()
+        df_comm = get_comm_df()
+        
+        # Concatenar
+        df = pd.concat([df_btc, df_comm], ignore_index=True)
+
+        # Inserir no banco de dados
+        df.to_sql('Cotacoes', engine, if_exists='append', index=False)
+
+        print("Dados inseridos com sucesso!")
+
+        # Esperar próximo ciclo
+        time.sleep(SLEEP_SECONDS)
